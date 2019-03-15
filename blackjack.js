198f27b4fe18ec23052aa1e388a644dcda6bfc0b
Carlos Oporto <carlosoporto@gmail.com>
2019-03-15 21:10:14 +0100

    Initial commit

---
+++ blackjack.js
@@ -0,0 +1,28 @@
+//
+// Blackjack by itos
+//
+
+let suits = ['Hearts', 'Clubs', 'Spades', 'Diamonds'];
+let values = ['Ace', 'King', 'Queen', 'Jack',
+    'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+    'Five', 'Four', 'Three', 'Two'];
+
+let deck = [];
+
+for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+      deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+    }
+}
+
+for (i = 0; i < deck.length; i++) {
+  console.log(deck[i]);
+}
+
+let playerCards = [ deck[0], deck [2] ];
+
+console.log("Welcome to Blackjack!");
+
+console.log("You are dealt: ");
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
