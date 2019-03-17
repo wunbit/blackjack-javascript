9182a0499a7fa4f857b985646605d3f754369fb7
Carlos Oporto <carlosoporto@gmail.com>
2019-03-17 19:12:05 +0100

    added card Object

--- blackjack.js
+++ blackjack.js
@@ -11,10 +11,18 @@ function createDeck() {
   let deck = [];
   for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
       for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-        deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+        let card = {
+          suit: suits[suitIdx],
+          value: values[valueIdx]
+        }
+        deck.push(card);
       }
   }
   return deck;
+}
+
+function getCardString (card) {
+  return card.value + ' of ' + card.suit;
 }
 
 function getNextCard() {
@@ -23,14 +31,10 @@ function getNextCard() {
 
 let deck = createDeck();
 
-for (i = 0; i < deck.length; i++) {
-  console.log(deck[i]);
-}
-
 let playerCards = [ getNextCard(), getNextCard() ];
 
 console.log("Welcome to Blackjack!");
 
 console.log("You are dealt: ");
-console.log(" " + playerCards[0]);
-console.log(" " + playerCards[1]);
+console.log(" " + getCardString(playerCards[0]));
+console.log(" " + getCardString(playerCards[1]));
